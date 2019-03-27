6cb1f22a9952eab62232e40738f3fbff715ae51c
falstack <icesilt@outlook.com>
2019-03-27 20:05:48 +0800

    first push

---
+++ examples/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+    <VueRollList></VueRollList>
+  </div>
+</template>
+
+<script>
+import VueRollList from '../src/VueRollList'
+
+export default {
+  name: 'app',
+  components: {
+    VueRollList
+  }
+}
+</script>
---
+++ src/VueRollList.vue
@@ -0,0 +1,23 @@
+<style lang="scss"></style>
+
+<template>
+  <div class="vue-roll-list">
+    VueRollList
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueRollList',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
